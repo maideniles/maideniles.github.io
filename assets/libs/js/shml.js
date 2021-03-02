f1a99333d1e159886b356f61f2315b574c7e974e
SteveBeeblebrox <stevebeeblebrox@users.noreply.github.com>
2021-03-02 16:32:09 -0700

    Updated SHML

--- assets/libs/js/shml.js
+++ assets/libs/js/shml.js
@@ -49,7 +49,6 @@ class SHML {
           .replace(/(,,)(.*?)\1/gs, '<sub>$2</sub>')
           .replace(/(\|)(.*?)\1/gs, '<mark>$2</mark>')
           .replace(/(\S)--(\S)/g, '$1<wbr>$2')
-          .replace(/(`)(.*?)\1/g, '<code>$2</code>')
           .replace(/\+\[(.*?)\]\((.*?)\)/g, '<a href="$2" title="$1" target="_blank">$1</a>')
           .replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2" title="$1" target="_self">$1</a>')
           .replace(/%%/g, '<br>')
@@ -62,6 +61,7 @@ class SHML {
       __proto__: null,
       _properties: {},
       _value: [],
+      _ids: [],
       toHTML: () => data._value.join(''),
       getProperty: (property) => data._properties[Symbol.for(property)],
       getProperties: () => new Proxy({__proto__: null, ...data._properties},
@@ -71,33 +71,56 @@ class SHML {
             for(let prop of Object.getOwnPropertySymbols(target).filter(object => object !== Symbol.iterator)) yield [prop, target[prop]]
           }).bind(target);
           else return target[Symbol.for(name)];
+        },
+        set: function() {
+            return false;
+        }
+      }),
+      getIds: () => new Proxy([...data._ids],
+      {
+        set: function() {
+            return false;
         }
       })
     };
     let push = object => data._value.push(object);
+    let pushId = object => data._ids.push(object);
     let parseForHeader = (header, str) => str.replace(new RegExp('^\\s*?' + '#'.repeat(header) + '(.*)', 'g'), (str, match) => (push('<h' + header + '>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</h' + header + '>'), ''));
+    let parseForIdHeader = (header, str) => str.replace(new RegExp('^\\s*?' + '#'.repeat(header) + '\\[(.*?)\\]\\s*?(.*)', 'g'), (str, match1, match2) => (pushId('h' + header + ':' + match1), push('<a href="#h' + header + ':' + match1 + '"><h' + header + ' id="h' + header + ':' + match1 + '">' + SHML.parseInlineMarkup(match2.trim()).toHTML() + '</h' + header + '></a>'), ''));
     let parseForSection = (tag, str, key = tag) => str.replace(new RegExp('^\\s*?' + key + ':(.*)', 'g'), (str, match) => (push('<' + tag + '>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</' + tag + '>'), ''));
-    let escaped = false;
+    let parseForIdSection = (tag, str, key = tag) => str.replace(new RegExp('^\\s*?' + key + '\\[(.*?)\\]:(.*)', 'g'), (str, match1, match2) => (pushId(tag + ':' + match1), push('<a href="#' + tag + ':' + match1 + '"><' + tag + ' id="' + tag + ':' + match1 + '">' + SHML.parseInlineMarkup(match2.trim()).toHTML() + '</' + tag + '></a>'), ''));
+    let escaped = false, table = false, tableHeader = true;
     markdown.split(/\n/g).forEach((object, index, array) => {
       if(object.trim() === '$$') return void (escaped = !escaped);
       if(escaped) return void push(object);
-      if(object.trim().startsWith('<') && object.trim().endsWith('>')) push(object);
-      else {
-        for(let i = 6; i > 0; i--) object = parseForHeader(i, object);
-        for(let i = 1; i < 7; i++) object = parseForSection('h' + i, object);
-        object = parseForSection('p', object);
-        object = object
-        .replace(/^\s*?!!(.*)/g, (str, match) => '')
-        .replace(/^\s*?!(.*?):(.*)/g, (str, match1, match2) => (data._properties[Symbol.for(match1)] ??= match2.trim(), ''))
-        .replace(/^\s*?>>(.*)/g, (str, match) => (push('<blockquote>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</blockquote>'), ''))
-        .replace(/^\s*?(?:bull:|\+)(.*)/g, (str, match) => (push('<ul><li>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</li></ul>'), ''))
-        .replace(/^\s*?\[(.*)\((.*?) ([0-9]*)[xX]([0-9]*)\)\]/g, (str, match1, match2, match3, match4) => (push('<img src="' + match2 + '" alt="' + match1.trim() + '" width="' + (parseInt(match3) === 0 ? 'auto' : match3) + '" height="' + (parseInt(match4) === 0 ? 'auto' : match4) + '">'), ''))
-        .replace(/^\s*?\[(.*)\((.*)\)\]/g, (str, match1, match2) => (push('<img src="' + match2 + '" alt="' + match1.trim() + '">'), ''))
-        .replace(/^\s*?\[(.*?) ([0-9]*)[xX]([0-9]*)\]/g, (str, match1, match2, match3) => (push('<img src="' + match1 + '" width="' + (parseInt(match2) === 0 ? 'auto' : match2) + '" height="' + (parseInt(match3) === 0 ? 'auto' : match3) + '">'), ''))
-        .replace(/^\s*?\[(.*)\]/g, (str, match) => (push('<img src="' + match + '">'), ''))
-        .replace(/\s*---+\s*/, () => (push('<hr>'), ''))
-        .replace(/\s*%%\s*/, () => (push('<br>'), ''));
+      
+      if(object.trim() === '[[') return void (table = true, push('<table>'));
+      else if(object.trim() === ']]') return void (table = false, tableHeader = true, push('</table>'));
+      else if(table) {
+        let makeRow = (t) => SHML.parseInlineMarkup(('<tr><t'+t+'>'+object.trim().split(/(?<!\$),/).join('</t'+t+'><t'+t+'>')+'</t'+t+'></tr>').replace(/\$,/g, ',')).toHTML();
+        if(tableHeader) return void (tableHeader = false, push(makeRow('h')));
+        else return void push(makeRow('d'));
       }
+      
+      
+      for(let i = 6; i > 0; i--) object = parseForIdHeader(i, object);
+      for(let i = 1; i < 7; i++) object = parseForIdSection('h' + i, object);
+      
+      for(let i = 6; i > 0; i--) object = parseForHeader(i, object);
+      for(let i = 1; i < 7; i++) object = parseForSection('h' + i, object);
+      object = parseForSection('p', object);
+      object = object
+      .replace(/^\s*?!!(.*)/g, (str, match) => '')
+      .replace(/^\s*?!(.*?):(.*)/g, (str, match1, match2) => (data._properties[Symbol.for(match1)] ??= match2.trim(), ''))
+      .replace(/^\s*?>>(.*)/g, (str, match) => (push('<blockquote>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</blockquote>'), ''))
+      .replace(/^\s*?(?:bull:|\+)(.*)/g, (str, match) => (push('<ul><li>' + SHML.parseInlineMarkup(match.trim()).toHTML() + '</li></ul>'), ''))
+      .replace(/^\s*?\[(.*)\((.*?) ([0-9]*)[xX]([0-9]*)\)\]/g, (str, match1, match2, match3, match4) => (push('<br><img src="' + match2 + '" alt="' + match1.trim() + '" width="' + (parseInt(match3) === 0 ? 'auto' : match3) + '" height="' + (parseInt(match4) === 0 ? 'auto' : match4) + '"><br>'), ''))
+      .replace(/^\s*?\[(.*)\((.*)\)\]/g, (str, match1, match2) => (push('<br><img src="' + match2 + '" alt="' + match1.trim() + '"><br>'), ''))
+      .replace(/^\s*?\[(.*?) ([0-9]*)[xX]([0-9]*)\]/g, (str, match1, match2, match3) => (push('<br><img src="' + match1 + '" width="' + (parseInt(match2) === 0 ? 'auto' : match2) + '" height="' + (parseInt(match3) === 0 ? 'auto' : match3) + '"><br>'), ''))
+      .replace(/^\s*?\[(.*)\]/g, (str, match) => (push('<br><img src="' + match + '"><br>'), ''))
+      .replace(/\s*---+\s*/, () => (push('<hr>'), ''))
+      .replace(/\s*%%\s*/, () => (push('<br>'), ''));
+      push(SHML.parseInlineMarkup(object.trim()).toHTML());
     });
     return data;
   }
